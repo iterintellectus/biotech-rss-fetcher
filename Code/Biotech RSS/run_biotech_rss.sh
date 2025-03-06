c933b9a33e24b3ba419880da37125614b626bdce
iterintellectus <57105299+iterintellectus@users.noreply.github.com>
2025-03-05 21:18:52 -0500

    Add shell script and crontab entry for automated runs

---
+++ Code/Biotech RSS/run_biotech_rss.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Navigate to the script directory
+cd "/Users/vittoriomottini/Documents/Bio-Acc/Code/Biotech RSS"
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Run the Python script
+python3 app.py
+
+# Deactivate the virtual environment
+deactivate 
